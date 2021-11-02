587a5bf04199b30d36547b917a83071153580565
David <DEvangelista54@gmail.com>
2021-11-02 18:38:27 -0400

    commit tree 6b0990ce06b4947edd2c48fc5a26460f1e6c252a

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -12,6 +12,18 @@ class PlantsController < ApplicationController
     render json: plant
   end
 
+  def update
+    plant = find_plant
+    plant.update(plant_params)
+    render json: plant
+  end
+
+  def destroy
+    plant = find_plant
+    plant.destroy
+    head :no_content
+  end
+
   # POST /plants
   def create
     plant = Plant.create(plant_params)
@@ -19,6 +31,10 @@ class PlantsController < ApplicationController
   end
 
   private
+
+  def find_plant
+    Plant.find(params[:id])
+  end
 
   def plant_params
     params.permit(:name, :image, :price, :is_in_stock)
